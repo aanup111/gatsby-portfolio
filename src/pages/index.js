828647d0c2794c6103fad6d31cb28d30d2fce9ed
Anup Ghimire <aanup111@gmail.com>
2021-05-02 20:36:04 -0700

    deployment-bug-fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import * as React from "react"
 import { useState } from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import Main from "../components/Main"
 import Projects from "../components/Projects"
-import Seo from "../components/Seo"
+import Seo from "../components/seo"
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import About from "../components/About"
 import Contact from "../components/Contact"
 import Dropdown from "../components/Dropdown"
-import Header from "../components/Header"
+import Header from "../components/header"
 
 const IndexPage = () => {
   const [isOpen, setIsOpen] = useState(false)
