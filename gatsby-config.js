a3c62b18e8704e786cde1bed7f1e36bef4144fd0
Anup Ghimire <aanup111@gmail.com>
2021-05-02 23:57:23 -0700

    home-message-changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Anup Ghimire Portfolio`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Anup Ghimire`,
+    description: `Come check out some of my work!`,
     author: `@gatsbyjs`,
   },
   plugins: [
